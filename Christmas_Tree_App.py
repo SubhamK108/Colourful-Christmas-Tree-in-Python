f35b2faee658f7cfc074ff2058bb5bfd0c89bfa9
Subham Karmakar <subhamk.subs@gmail.com>
2020-02-09 17:34:34 +0530

    Tweaked the timing of refresh

--- Christmas_Tree_App.py
+++ Christmas_Tree_App.py
@@ -36,7 +36,7 @@ def LightUp(colour, indices):
             lock.release()
 
             Off = not Off
-            time.sleep(random.uniform(0.05, 0.1))
+            time.sleep(random.uniform(0.4, 0.5))
 
 
 Red = []
